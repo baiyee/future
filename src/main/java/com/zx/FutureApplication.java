426b3967ba80c71d7760bfbfb53b21ac9ad22fae
theshy <theshy.zhang@jtexpress.com>
2022-12-11 20:50:00 +0800

    集成logback日志

--- src/main/java/com/zx/FutureApplication.java
+++ src/main/java/com/zx/FutureApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhangxuan
+ */
 @MapperScan("com.zx.mapper")
 @SpringBootApplication
 public class FutureApplication {
